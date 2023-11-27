07a762c6b5e8e9f0d1f4a884dd0ebf39f7f28533
Dhiah Ayu <imdevender89@gmail.com>
2023-11-27 09:34:49 +0700

    Fix Merge All

--- resources/views/homeproject.blade.php
+++ resources/views/homeproject.blade.php
@@ -3,5 +3,51 @@
 @section('content')
     <div class="content-wrapper">
         <h1 class="text-center" style="margin-top: 1em">HALAMAN HOME UNTUK ROLE PROJECT</h1>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="card col-10 mx-auto">
+                        <div class="card-body">
+                            <table class="table"  style="width: 100%">
+                                <thead>
+                                    <tr style="text-align: center">
+                                        <th scope="col">No</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Application Name</th>
+                                        <th scope="col">Data Owner</th>
+                                        <th scope="col">Progress</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $no = 1;
+                                    @endphp
+                                    @foreach ($data as $row)
+                                    @if ($row->kirimke === 'Planning')
+                                    <tr style="text-align: center; align-items: center;">
+                                        <th scope="row">{{$no++}}</th>
+                                        <td>{{ date('d/m/Y', strtotime($row->created_at)) }}</td>
+                                        <td>{{$row->project_name}}</td>
+                                        <td>{{$row->data_owner}}</td>
+                                        <td>
+                                            {{-- {{$row->data_owner}} --}}
+                                            blm ada atributnya di tabel FormRequest
+                                        </td>
+                                        <td>
+                                            <a href="/viewcra/{{$row->id}}" class="btn btn-info"><i class="fas fa-solid fa-eye" style="color: #ffffff;"></i></a>
+                                            {{-- BTN EXPORT PDF --}}
+                                            {{-- <a target="_blank" href="/download_pdf/{{$row->id}}" class="btn btn-success mb-1"><i class="fas fa-file-pdf" style="color: #ffffff;"></i></a> --}}
+                                        </td>
+                                    </tr>
+                                    @endif
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div><!-- /.row -->
+                    </div><!-- /.container-fluid -->
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
